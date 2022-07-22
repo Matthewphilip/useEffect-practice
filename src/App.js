6624cafe8e77067189a3b677f5f2aa394a424425
Matthew Wiggins <mattwiggins5@gmail.com>
2022-07-22 16:27:20 +0100

    added setTimout function and clearTimout function to the setFormIsValid useEffect function so the setFormIsValid function only runs once rather than everytime a new key is entered (state changes)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []); // if we keep the dependancies array empty then the useEffect will only run once when the page is loaded (i.e it would keep you logged in and you wouldn't need to log in each time the page refreshed), if we entered dependancies in the array the useEffect would run everytime the dependancies (or state) changed
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn", "0");
     setIsLoggedIn(false);
   };
 
